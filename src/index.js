f4170581e05ae8a87085641438aa17d23131a9ed
natsu-kim <nevernet@empal.com>
2022-03-18 11:45:52 +0900

    public init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactGA from "react-ga4";
 import { render } from 'react-dom';
 import { createBrowserHistory } from 'history'
 import { Provider } from 'react-redux';
@@ -25,9 +24,6 @@ const store = createStore(
     )
   )
 );
-ReactGA.initialize("G-CFVPFRLN6J");
-ReactGA.send("pageview");
-
 
 render(
   <ReactKeycloakProvider authClient={keycloak}>
